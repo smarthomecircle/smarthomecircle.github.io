95a169108a36b3c38bf1c079f1e51f44669a0ebf
smarthomecircle <smarthomecircle@gmail.com>
2023-12-24 16:20:56 +0100

    Added blog

--- components/PostShare.js
+++ components/PostShare.js
@@ -37,6 +37,7 @@ const ShareButtons = ({ url, title }) => {
 
   return (
     <div>
+    <br/>
       <button
         type="button"
         className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
