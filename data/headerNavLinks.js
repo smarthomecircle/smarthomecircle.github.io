de331925a57b566b314ec973a55078c369ba372e
smarthomecircle <smarthomecircle@gmail.com>
2024-06-07 13:46:20 +0200

    Adding youtube header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
+  { href: 'https://www.youtube.com/@SmartHomeCircle', title: 'YouTube' },
   { href: '/tags', title: 'Tags' },
   { href: '/posts', title: 'Posts' },
   { href: '/about', title: 'About & Contact' },
