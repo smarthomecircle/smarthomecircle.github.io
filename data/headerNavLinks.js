bb8094ff624ef869cc4e7b6cdcd8962d58b1ec18
smarthomecircle <smarthomecircle@gmail.com>
2025-02-19 11:56:56 +0100

    Adding devices page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: 'https://www.youtube.com/@SmartHomeCircle', title: 'YouTube' },
   { href: '/tags', title: 'Tags' },
   { href: '/posts', title: 'Posts' },
+  { href: '/sbc', title: 'SBC' },
   { href: '/about', title: 'About & Contact' },
 ]
 
