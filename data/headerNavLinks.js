c12c71449c9332fecfeb2cb1a5c1b9157b931eaa
smarthomecircle <smarthomecircle@gmail.com>
2025-06-02 21:05:59 +0200

    Added the drop down menu

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,17 @@
+
 const headerNavLinks = [
-  // { href: '/blog', title: 'Blog' },
   { href: 'https://www.youtube.com/@SmartHomeCircle', title: 'YouTube' },
   { href: '/tags', title: 'Tags' },
   { href: '/posts', title: 'Posts' },
-  { href: '/sbc', title: 'SBC' },
+  {
+    title: 'Devices',
+    children: [
+      { href: '/sbc', title: 'Single Board Computer' },
+      { href: '/devices/beaglebone', title: 'BeagleBone' },
+      { href: '/devices/arduino', title: 'Arduino' },
+    ],
+  },
+  // { href: '/sbc', title: 'SBC' },
   { href: '/about', title: 'About & Contact' },
 ]
 
