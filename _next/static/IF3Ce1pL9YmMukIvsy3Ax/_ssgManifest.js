4a92a0cb780ae993cc26227081f7fa0753c82e97
smarthomecircle <smarthomecircle@users.noreply.github.com>
2025-11-06 10:11:07 +0000

    deploy: ce062ddc424404f322331c1897769cc2b4da5606

--- _next/static/IF3Ce1pL9YmMukIvsy3Ax/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[slug]","\u002Fabout","\u002Fpage\u002F[page]","\u002Fposts","\u002Fprivacy","\u002Fprojects","\u002Ftags","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
