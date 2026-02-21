2dfe2b0909aa7c911baa84b74857d64b609c807c
smarthomecircle <smarthomecircle@gmail.com>
2026-02-21 14:50:24 +0100

    updated page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,16 +1,16 @@
-
+// Optional: add `badge: 'New'` (or any string) to any top-level link to show a superscript label
 const headerNavLinks = [
   { href: 'https://www.youtube.com/@SmartHomeCircle', title: 'YouTube' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/posts', title: 'Posts' },
-  { href: '/sbc-compare', title: 'Compare SBC' },
+  { href: '/sbc-compare', title: 'Compare SBC', badge: 'New' },
   {
     title: 'Devices',
     children: [
       { href: '/sbc', title: 'Single Board Computer' },
     ],
   },
-  { href: '/sale', title: 'Sale' },
+  // { href: '/sale', title: 'Sale' },
   { href: '/about', title: 'About & Contact' },
   {
     title: 'More',
